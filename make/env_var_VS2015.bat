29beeafa0d7749191b5f7610b2303b76bf94b875
umaur <43574967+umaur@users.noreply.github.com>
2018-09-28 13:55:48 +0200

    Add files via upload

---
+++ make/env_var_VS2015.bat
@@ -0,0 +1,3 @@
+@echo off 
+set "VSCMD_START_DIR=%cd%"
+cmd.exe /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat""
---
+++ make/env_var_VS2017.bat
@@ -0,0 +1,4 @@
+@echo off 
+set "VSCMD_START_DIR=%cd%"
+cmd.exe /k ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat""
+
